b48e016c6afeb7589fa0b552366565e0afad6bd6
Profuturo AFP <daniel.salhuana@avantica.net>
2019-07-15 15:35:12 -0500

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'City Guide App',
+      title: 'City Guide App ',
       home: StartScreen(),
       theme: ThemeData(
         primaryColor: Colors.blue,
